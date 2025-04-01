b2a486482d8cfdba7a38c21be7e2c6c2eaa5fffe
robert <robert.gjorgievski1987@gmail.com>
2025-04-01 16:11:52 +0200

    Initial commit

---
+++ src/main/java/com/airport/mk/aeroplane/AeroplaneApplication.java
@@ -0,0 +1,13 @@
+package com.airport.mk.aeroplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroplaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airport/mk/aeroplane/AeroplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.mk.aeroplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
